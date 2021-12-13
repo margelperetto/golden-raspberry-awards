c2ae53ac1ab8e06fd9a5c5a7a604087327c0d781
margelperetto <margel.peretto@gmail.com>
2021-12-13 00:36:14 -0300

    Rest controller adjustment

--- src/main/java/br/com/margel/goldenraspberryawards/resources/AwardsAnalizerRestCtrl.java
+++ src/main/java/br/com/margel/goldenraspberryawards/resources/AwardsAnalizerRestCtrl.java
@@ -1,7 +1,6 @@
 package br.com.margel.goldenraspberryawards.resources;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.margel.goldenraspberryawards.business.AwardsAnalizer;
@@ -11,7 +10,7 @@ import br.com.margel.goldenraspberryawards.model.MinMax;
 public class AwardsAnalizerRestCtrl {
 
 	@GetMapping("calc-min-max")
-	public MinMax calcMinMax(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public MinMax calcMinMax() {
 		return new AwardsAnalizer().calcMinMax();
 	}
 
